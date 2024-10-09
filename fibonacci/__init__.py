ac419301e49c615906237e48f4536f3474c53c01
Yuxin Gong <yg2523@ic.ac.uk>
2024-10-09 12:02:13 +0100

    initial fibonacci package implementation

---
+++ fibonacci/__init__.py
@@ -0,0 +1,2 @@
+from .fibonacci import fib 
+from .fibonacci_sequence import fib_seq
---
+++ fibonacci/fibonacci_sequence.py
@@ -0,0 +1,6 @@
+import fibonacci
+def fib_seq(n):
+    if n == 0:
+        return [0]
+    else:
+        return fib_seq(n - 1) + [fibonacci.fib(n)]
